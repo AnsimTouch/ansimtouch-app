ae4c4ebf44eb58d13d01d3a02338c55803cd6ad5
bbqcream <imground11@gmail.com>
2025-01-02 10:51:28 +0900

    fix :: navigator 오류 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,42 @@
-import * as SplashScreen from "expo-splash-screen";
-import "react-native-reanimated";
+import React, { useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-
-import User from "./(tabs)/user";
 import AddUser from "./(tabs)/addUser";
+import User from "./(tabs)/user";
+import NotFound from "./(tabs)/notFound";
+import { useNavigation } from "@react-navigation/native";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 const Stack = createStackNavigator();
+
 export default function RootLayout() {
+  const navigation = useNavigation();
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("state", (e) => {
+      const routeNames = e.data.state.routeNames;
+      const currentRoute = e.data.state.routes[e.data.state.index].name;
+
+      if (!routeNames.includes(currentRoute)) {
+        navigation.navigate("NotFound");
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <Stack.Navigator initialRouteName="User">
+      <Stack.Screen
+        name="AddUser"
+        component={AddUser}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen
         name="User"
         component={User}
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name="AddUser"
-        component={AddUser}
+        name="NotFound"
+        component={NotFound}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
