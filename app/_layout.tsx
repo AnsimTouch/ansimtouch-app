b4a8e9ed2c0fc59b37b02d66b8b1736bd2d3e606
김성한 <imground11@gmail.com>
2025-01-02 12:29:52 +0900

    fix :: notFound수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,25 +3,10 @@ import { createStackNavigator } from "@react-navigation/stack";
 import AddUser from "./(tabs)/addUser";
 import User from "./(tabs)/user";
 import NotFound from "./(tabs)/notFound";
-import { useNavigation } from "@react-navigation/native";
 
 const Stack = createStackNavigator();
 
 export default function RootLayout() {
-  const navigation = useNavigation();
-  useEffect(() => {
-    const unsubscribe = navigation.addListener("state", (e) => {
-      const routeNames = e.data.state.routeNames;
-      const currentRoute = e.data.state.routes[e.data.state.index].name;
-
-      if (!routeNames.includes(currentRoute)) {
-        navigation.navigate("NotFound");
-      }
-    });
-
-    return unsubscribe;
-  }, [navigation]);
-
   return (
     <Stack.Navigator initialRouteName="User">
       <Stack.Screen
