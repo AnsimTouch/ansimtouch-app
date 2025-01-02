6b1bafe97336a817523b01028a8fd1e626263e2c
bbqcream <imground11@gmail.com>
2025-01-02 22:14:53 +0900

    feat :: modal 구현

--- app/(tabs)/emergency.tsx
+++ app/(tabs)/emergency.tsx
@@ -1,12 +1,89 @@
-import { SafeAreaView, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  Modal,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import * as S from "../../style/emergency";
 import Nav from "@/components/Nav/nav";
+import { useEffect, useState } from "react";
 
 export default function Emergency() {
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  useEffect(() => {}, []);
+
+  const onPressModalOpen = () => {
+    console.log("팝업을 여는 중입니다.");
+    setIsModalVisible(true);
+  };
+
+  const onPressModalClose = () => {
+    setIsModalVisible(false);
+  };
   return (
     <S.Container>
-      <Nav title="유저 관리" router="User" />
-      <Text>안ㄴ</Text>
+      <Nav title="긴급 알림" router="User" />
+      <S.MainWrapper>
+        <S.ContentWrapper>
+          <Image source={require("../../assets/images/Emergency.png")} />
+          <Text style={styles.text}>긴급 알림을 실행합니다.</Text>
+          <S.Button onPress={onPressModalOpen}>
+            <Text style={styles.buttonText}>긴급 알림 실행</Text>
+          </S.Button>
+        </S.ContentWrapper>
+      </S.MainWrapper>
+      <S.ModalView>
+        <Modal animationType="fade" visible={isModalVisible} transparent={true}>
+          <S.ModalContainer>
+            <S.ModalWrapper>
+              <S.ModalText>
+                <Text style={styles.modalTitle}>
+                  정말로 알림을{`\n`}실행하시겠습니까?
+                </Text>
+                <Text style={styles.modalDetail}>
+                  해당 알림은 취소 할 수 없습니다.
+                </Text>
+              </S.ModalText>
+              <S.ButtonWrapper>
+                <S.ModalButton onPress={onPressModalClose} color="#D9D9D9">
+                  <Text style={styles.modalCancelText}>취소</Text>
+                </S.ModalButton>
+                <S.ModalButton color="#FF3232">
+                  <Text style={styles.modalConfirmText}>실행</Text>
+                </S.ModalButton>
+              </S.ButtonWrapper>
+            </S.ModalWrapper>
+          </S.ModalContainer>
+        </Modal>
+      </S.ModalView>
     </S.Container>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: 700,
+  },
+  modalCancelText: {
+    fontSize: 18,
+  },
+  modalConfirmText: {
+    fontSize: 18,
+    color: "white",
+  },
+  modalTitle: {
+    fontSize: 22,
+    fontWeight: 600,
+  },
+  modalDetail: {
+    fontSize: 16,
+  },
+});
--- style/emergency.ts
+++ style/emergency.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components/native";
 
+interface colorProps {
+  color: string;
+}
+
 export const Box = styled.View`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
 `;
 
@@ -13,4 +17,70 @@ export const Container = styled.SafeAreaView`
   align-items: center;
   flex-direction: column;
   background-color: #fbfdff;
+`;
+
+export const MainWrapper = styled(Box)`
+  margin-top: -60px;
+  width: 100%;
+  height: 100%;
+`;
+
+export const ContentWrapper = styled(Box)`
+  flex-direction: column;
+  width: 100%;
+  height: 353px;
+  background-color: white;
+  border-radius: 5px;
+  gap: 18px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  width: 60%;
+  background-color: #ff3232;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  border-radius: 5px;
+`;
+
+export const ModalView = styled(Box)`
+  width: 100%;
+  height: 100%;
+  background-color: "black";
+`;
+
+export const ModalContainer = styled(Box)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(1, 1, 1, 0.3);
+`;
+
+export const ModalWrapper = styled(Box)`
+  width: 80%;
+  height: 183px;
+  background-color: white;
+  border-radius: 20px;
+  justify-content: space-evenly;
+`;
+
+export const ModalText = styled(Box)`
+  width: 85%;
+  align-items: flex-start;
+`;
+
+export const ButtonWrapper = styled(Box)`
+  width: 85%;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const ModalButton = styled.TouchableOpacity<colorProps>`
+  width: 48%;
+  height: 54px;
+  border-radius: 5px;
+  background-color: ${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
