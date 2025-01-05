8cd5b68aeb63c557a036239ac5c4f9cd90a7093c
h-0y28 <s24015@gsm.hs.kr>
2025-01-05 15:30:54 +0900

    style: register 디자인 반영영

--- app/(tabs)/Register.tsx
+++ app/(tabs)/Register.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Text, TextInput, StyleSheet, Pressable, View } from "react-native";
-import * as S from "../../style/auth";
+import axios from "axios";
 import { useNavigation } from "expo-router";
 import { StackNavigationProp } from "@react-navigation/stack";
 import Nav from "@/components/Nav/nav";
+import SelectModal from "@/components/Modal/auth";
+import * as S from "../../style/auth";
+import styled from "styled-components/native";
 
 type RootStackParamList = {
   Login: undefined;
@@ -12,70 +14,158 @@ type RootStackParamList = {
 
 export default function Register() {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [verificationCode, setVerificationCode] = useState<string>("");
+  const [isVerificationStarted, setIsVerificationStarted] =
+    useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [modalMessage, setModalMessage] = useState<string>("");
 
-  const onRegister = () => {
-    if (email && password) {
-      alert("회원가입 성공!");
-      navigation.navigate("Login");
-    } else {
-      alert("모든 필드를 입력하세요.");
+  const apiKey = "skjdklas"; // 임시 API 키
+
+  const onRegister = async () => {
+    if (
+      !name ||
+      !password ||
+      !confirmPassword ||
+      !phoneNumber ||
+      !verificationCode
+    ) {
+      setErrorMessage("모든 필드를 입력하세요.");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setErrorMessage("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    setIsLoading(true);
+    try {
+      const response = await axios.post(`https://${apiKey}/register`, {
+        name,
+        password,
+        phoneNumber,
+        verificationCode,
+      });
+      if (response.status === 200) {
+        setIsLoading(false);
+        setModalMessage("회원가입 성공!");
+        setIsModalVisible(true);
+        setTimeout(() => navigation.navigate("Login"), 2000);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      setModalMessage("회원가입 실패! 다시 시도해주세요.");
+      setIsModalVisible(true);
     }
   };
 
+  const onRequestVerification = () => {
+    if (!phoneNumber) {
+      setErrorMessage("전화번호를 입력해주세요.");
+      return;
+    }
+
+    setErrorMessage("");
+    setIsVerificationStarted(true);
+
+    axios
+      .post(`https://${apiKey}/send-verification-code`, { phoneNumber })
+      .then(() => {
+        setModalMessage("인증 코드가 발송되었습니다.");
+        setIsModalVisible(true);
+      })
+      .catch(() => {
+        setModalMessage("인증 코드 발송에 실패했습니다. 다시 시도해주세요.");
+        setIsModalVisible(true);
+      });
+  };
+
   return (
-    <View style={S.styles.container}>
+    <S.AuthContainer>
       <Nav title="회원가입" router="Register" />
-      <View style={S.styles.mainWrapper}>
-        <Text style={styles.title}>회원가입</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="이메일"
-          placeholderTextColor="#aaa"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
+
+      <S.MainWrapper>
+        <S.Title>회원가입</S.Title>
+        <S.SubTitle>회원가입 후 진행해주세요.</S.SubTitle>
+
+        <S.Input
+          placeholder="이름을 입력해주세요."
+          placeholderTextColor="#4C4C4C"
+          value={name}
+          onChangeText={setName}
         />
-        <TextInput
-          style={styles.input}
-          placeholder="비밀번호"
-          placeholderTextColor="#aaa"
+        <S.Input
+          placeholder="비밀번호를 입력해주세요."
+          placeholderTextColor="#4C4C4C"
           secureTextEntry
           value={password}
-          onChangeText={(text) => setPassword(text)}
+          onChangeText={setPassword}
         />
-        <Pressable style={styles.button} onPress={onRegister}>
-          <Text style={styles.buttonText}>회원가입</Text>
-        </Pressable>
-      </View>
-    </View>
+        <S.Input
+          placeholder="비밀번호를 재입력해주세요."
+          placeholderTextColor="#4C4C4C"
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+        <PhoneInputWrapper>
+          <S.Input
+            placeholder="전화번호를 입력해주세요."
+            placeholderTextColor="#4C4C4C"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+            keyboardType="numeric"
+            style={{ flex: 1 }}
+          ></S.Input>
+          <PhoneButton onPress={onRequestVerification}>인증하기</PhoneButton>
+        </PhoneInputWrapper>
+        {isVerificationStarted && (
+          <S.Input
+            placeholder="인증코드를 입력해주세요."
+            placeholderTextColor="#4C4C4C"
+            value={verificationCode}
+            onChangeText={setVerificationCode}
+          />
+        )}
+      </S.MainWrapper>
+
+      <S.Link onPress={() => navigation.navigate("Login")}>
+        이미 아이디가 있으신가요?
+      </S.Link>
+      {errorMessage ? <S.ErrorMessage>{errorMessage}</S.ErrorMessage> : null}
+
+      <S.Button onPress={onRegister} disabled={isLoading}>
+        <S.ButtonText>{isLoading ? "회원가입 중..." : "회원가입"}</S.ButtonText>
+      </S.Button>
+
+      <SelectModal
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+        title="알림"
+        detail={modalMessage}
+      />
+    </S.AuthContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  input: {
-    width: "100%",
-    height: 50,
-    borderWidth: 1,
-    borderColor: "#ddd",
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 15,
-  },
-  button: {
-    backgroundColor: "#007BFF",
-    padding: 15,
-    borderRadius: 5,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
+const PhoneInputWrapper = styled.View`
+  flex-direction: row;
+  margin-bottom: 10px;
+`;
+
+const PhoneButton = styled.TouchableOpacity`
+  position: absolute;
+  right: 0;
+  cursor: pointer;
+  color: #2882ff;
+  padding: 10px 15px;
+  border-radius: 5px;
+  margin-left: 10px;
+`;
--- style/auth.tsx
+++ style/auth.tsx
@@ -1,14 +1,66 @@
-import { StyleSheet } from "react-native";
+import styled from "styled-components/native";
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F5F5F5",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  mainWrapper: {
-    width: "90%",
-    alignItems: "center",
-  },
-});
+export const AuthContainer = styled.View`
+  background-color: #fbfdff;
+  flex: 1;
+  margin: 0;
+  padding: 1rem;
+  position: relative;
+`;
+
+export const MainWrapper = styled.View``;
+
+export const Title = styled.Text`
+  font-size: 1.75rem;
+  color: #2882ff;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 1.125rem;
+  margin-bottom: 1.25rem;
+`;
+
+export const Input = styled.TextInput`
+  width: 100%;
+  height: 3.125rem;
+  font-size: 12px;
+
+  background-color: #ffffff;
+  border-width: 1px;
+  border-color: #ededed;
+  border-radius: 5px;
+  padding-left: 10px;
+  margin-bottom: 10px;
+`;
+
+export const Link = styled.TouchableOpacity`
+  color: #2882ff;
+  font-size: 12px;
+  text-align: end;
+`;
+
+export const ErrorMessage = styled.Text`
+  color: #ff4d4d;
+  font-size: 14px;
+  margin-top: 8px;
+  text-align: center;
+`;
+
+export const Button = styled.Pressable`
+  background-color: #2882ff;
+  padding: 20.5px;
+  border-radius: 5px;
+  align-items: center;
+
+  position: absolute;
+  bottom: 20px;
+  left: 0;
+  right: 0;
+  margin: 0 20px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 1rem;
+  font-weight: bold;
+`;
