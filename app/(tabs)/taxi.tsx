93f27ffcb4ebcf5274988be910460bc06bfc6088
bbqcream <imground11@gmail.com>
2025-01-06 17:53:13 +0900

    feat :: 택시 마커 사용

--- app/(tabs)/taxi.tsx
+++ app/(tabs)/taxi.tsx
@@ -13,7 +13,7 @@ export default function Taxi() {
             <S.LocateText>이기철 님의 위치</S.LocateText>
             <S.LocateDetail>SK남산그린빌딩에 위치하고 있습니다.</S.LocateDetail>
           </S.TextWrapper>
-          <S.Button bgColor="#2882FF">
+          <S.Button>
             <S.ButtonText>택시 호출</S.ButtonText>
           </S.Button>
         </S.ContentsWrapper>
--- components/Map/map.tsx
+++ components/Map/map.tsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import MapView, { Marker, PROVIDER_DEFAULT, Region } from "react-native-maps";
 
 export default function Map() {
+  const [region, setRegion] = useState<Region>({
+    latitude: 37.541,
+    longitude: 126.986,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  const changeRegion = (newRegion: Region) => {
+    setRegion(newRegion);
+    console.log(region);
+  };
+
   return (
     <View style={styles.container}>
       <MapView
+        region={region}
+        onRegionChangeComplete={changeRegion}
         provider={PROVIDER_DEFAULT}
-        initialRegion={{
-          latitude: 37.541,
-          longitude: 126.986,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
         style={styles.map}
-      />
+      >
+        <Marker coordinate={region}></Marker>
+      </MapView>
     </View>
   );
 }
