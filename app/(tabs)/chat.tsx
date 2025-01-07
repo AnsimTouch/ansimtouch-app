93b19717c862fc8a4ad9b5f6eae7d79b29d5e4b7
bbqcream <imground11@gmail.com>
2025-01-07 20:11:45 +0900

    feat :: 채팅 post 완성

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -2,7 +2,11 @@ import * as S from "../../style/chat";
 import Nav from "@/components/Nav/nav";
 import MyChat from "@/components/ChatModel/myChat";
 import AiChat from "@/components/ChatModel/aiChat";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { SERVER_URL } from "@env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import {
   Image,
   Keyboard,
@@ -14,15 +18,63 @@ import {
 } from "react-native";
 
 export default function Chat() {
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  // accesstoken 들고오기
+  const loadAccessToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem("accesstoken");
+      if (token) {
+        setAccessToken(token);
+      } else {
+        console.error("Access token not found");
+      }
+    } catch (e) {
+      console.error("Failed to load access token", e);
+    }
+  };
+
+  useEffect(() => {
+    loadAccessToken();
+  }, []);
+
   const [chat, setChat] = useState<string>("");
+
+  const sendChat = async () => {
+    try {
+      if (!accessToken) {
+        console.error("Access token is not available");
+        return;
+      }
+
+      console.log(`${SERVER_URL}`);
+      const res = await axios.post(
+        `${SERVER_URL}chat/send`,
+        { message: chat },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (res) {
+        console.log("성공");
+        setChat("");
+      }
+    } catch (e) {
+      console.error("Error sending chat:", e);
+    }
+  };
+
   const handleChatChange = (
     e: NativeSyntheticEvent<TextInputChangeEventData>
   ) => {
     setChat(e.nativeEvent.text);
   };
+
   const day = new Date();
   const formattedDate = `${day.getHours()}:${day.getMinutes()}`;
-  const sendChat = () => {};
 
   return (
     <KeyboardAvoidingView
@@ -30,7 +82,6 @@ export default function Chat() {
       behavior={Platform.OS === "ios" ? "padding" : "height"} // 키보드 나오면 따라서 올라감
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        {/*밖에 누르먄 키보드 사라짐*/}
         <S.Container>
           <Nav title="채팅하기" router="Home" />
           <MyChat
@@ -46,6 +97,7 @@ export default function Chat() {
               placeholder="채팅을 입력해 주세요."
               onChange={handleChatChange}
               multiline={true}
+              value={chat}
             />
             <S.SendImage chat={chat} onPress={sendChat}>
               <Image source={require("../../assets/images/Send.png")} />
