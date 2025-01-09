02eb2e9ee1f953c6000174234815f0b9aaf595b9
bbqcream <imground11@gmail.com>
2025-01-09 09:02:49 +0900

    refactor :: main 구조 변동

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,8 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Index from "./(tabs)";
 
 const Stack = createStackNavigator();
 
 export default function RootLayout() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Index"
-        component={Index}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  );
+  return <Stack.Navigator initialRouteName="Home"></Stack.Navigator>;
 }
